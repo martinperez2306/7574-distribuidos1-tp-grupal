ba284a219cb163903480776e59e19adabc877d33
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-11-05 00:06:23 -0300

    Add watcher middleware

---
+++ tp1/watcher/src/middleware.py
@@ -0,0 +1,50 @@
+import logging
+from common.middleware import Middleware
+
+WATCHER_EXCHANGE = 'watcher_exchange'
+WATCHER_QUEUE = 'watcher_queue'
+
+logging.getLogger("pika").propagate = False
+
+class WatcherMiddlware(Middleware):
+    def __init__(self, service_id) -> None:
+        super().__init__()
+
+        self.channel.exchange_declare(exchange=WATCHER_EXCHANGE,
+                                      exchange_type='fanout')
+
+        queue=WATCHER_QUEUE + service_id
+        self.channel.queue_declare(queue)
+
+        self.channel.queue_bind(
+            exchange=WATCHER_EXCHANGE, queue=queue)
+
+        self.channel.basic_qos(prefetch_count=1)
+
+    def accept_heartbeats(self, callback):
+        # Get ten messages and break out
+        for method_frame, properties, body in self.channel.consume(queue=WATCHER_QUEUE, inactivity_timeout=10):
+
+            heartbeat = None
+
+            if method_frame is None and properties is None and body is None:
+                logging.info("Timeout for receive message")
+
+            else:
+                # Display the message parts
+                logging.info(method_frame)
+                logging.info(properties)
+                logging.info(body)
+                heartbeat = body.decode()
+                # Acknowledge the message
+                self.channel.basic_ack(method_frame.delivery_tag)
+
+            callback(heartbeat)
+
+    def stop(self):
+        # Cancel the consumer and return any pending messages
+        requeued_messages = self.channel.cancel()
+        logging.info('Requeued %i messages' % requeued_messages)
+        # Close the channel and the connection
+        self.channel.close()
+        self.connection.close()
--- tp1/watcher/src/watcher.py
+++ tp1/watcher/src/watcher.py
@@ -5,65 +5,34 @@ import pika
 import docker
 
 from src.heartbeats import Heartbeats
+from src.middleware import WatcherMiddlware
 
 WATCHER_QUEUE = "watcher_queue"
 
 class Watcher:
     def __init__(self) -> None:
-        self.connection = None
-        self.channel = None
-        self.tries = 0
         self.heartbeats = Heartbeats()
         self.docker = docker.from_env()
+        self.id = os.environ['HOSTNAME']
         signal.signal(signal.SIGTERM, self.exit_gracefully)
         signal.signal(signal.SIGINT, self.exit_gracefully)
-
+        self.middleware = WatcherMiddlware(self.id)
 
     def start(self):
-        host = os.environ['RABBIT_SERVER_ADDRESS']
+        self.middleware.accept_heartbeats(self.handle_heartbeat)
+        self.middleware.stop()
 
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host))
-
-        self.channel = self.connection.channel()
-
-        self.channel.queue_declare(
-                queue=WATCHER_QUEUE)
-
-        # Get ten messages and break out
-        for method_frame, properties, body in self.channel.consume(queue=WATCHER_QUEUE, inactivity_timeout=10):
-
-            if method_frame is None and properties is None and body is None:
-                logging.info("Timeout for receive message")
-
-            else:
-                # Display the message parts
-                logging.info(method_frame)
-                logging.info(properties)
-                logging.info(body)
-                self.heartbeats.hearbeat(body.decode())
-                # Acknowledge the message
-                self.channel.basic_ack(method_frame.delivery_tag)
-
-            unavailable_services = self.heartbeats.get_unavailable_services()
-            self.wake_up_services(unavailable_services)
-
-        self.close()
+    def handle_heartbeat(self, heartbeat):
+        self.heartbeats.hearbeat(heartbeat)
+        unavailable_services = self.heartbeats.get_unavailable_services()
+        self.wake_up_services(unavailable_services)
 
     def wake_up_services(self, unavailable_services: list):
         for service in unavailable_services:
             self.docker.api.stop(service)
             self.docker.api.start(service)
     
-    def close(self):
-        # Cancel the consumer and return any pending messages
-        requeued_messages = self.channel.cancel()
-        logging.info('Requeued %i messages' % requeued_messages)
-        # Close the channel and the connection
-        self.channel.close()
-        self.connection.close()
-
     def exit_gracefully(self, *args):
-        self.close()
+        self.middleware.stop()
         logging.info(
             'Exiting gracefully')
