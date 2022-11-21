8782732a76d43f37502ff32c4a4509ccc4140d51
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-11-21 14:38:40 -0300

    Remove unused constant

--- tp1/common/constants.py
+++ tp1/common/constants.py
@@ -1,6 +1,5 @@
 CATEGORY_SUBFIX = '_category_id.json'
 DATA_SUBFIX = '_youtube_trending_data.csv'
 
-NO_HEARTBEAT_CODE = -1
 HEARTBEAT_FRECUENCY = 5 #In seconds
 SERVICE_HEARTBEAT_TIMEOUT = 10 #In seconds
