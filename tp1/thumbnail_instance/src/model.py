dcc7819f0dae32f9a1a2ffdbfa9307e794109afe
Tomas Nocetti <tnocetti@fi.uba.ar>
2022-11-25 17:37:07 -0300

    feat: adapt thumnail_instance service to multiclient

--- tp1/thumbnail_instance/src/model.py
+++ tp1/thumbnail_instance/src/model.py
@@ -1,18 +1,48 @@
 import logging
 
+MIN_PERIOD_IN_DAYS = 21
+
+# video_ids
+# {
+#   [client_id]: {
+#       [video_id]: [date1, date2, date3]
+#   }
+# }
+
+# video_countries
+# {
+#   [client_id]: {
+#       [video_id]: [country1, country2, country3]
+#   }
+# }
 
 class ThumbnailGrouper:
-    def __init__(self, path):
-        self.path = path
-        self.video_unique = {}
+    def __init__(self):
+        self.video_ids = {}
         self.video_countries = {}
+        self.country_count = {}
+        self.processed = {}
 
-    def add_date(self, video_id, country, trending_date) -> bool:
+    def add_country_count(self, client_id, country_count):
+        self.country_count[client_id] = country_count
 
-        self.video_unique.setdefault(video_id, [])
-        self.video_countries.setdefault(video_id, [])
-        trending_dates = self.video_unique[video_id]
-        trending_countries = self.video_countries[video_id]
+    def add_date(self, client_id, video_id, country, trending_date) -> bool:
+        country_count = self.country_count[client_id]
+
+        self.processed.setdefault(client_id, set())
+        processed = self.processed[client_id]
+
+        self.video_ids.setdefault(client_id, {})
+        self.video_countries.setdefault(client_id, {})
+
+        video_ids = self.video_ids[client_id]
+        video_ids.setdefault(video_id, [])
+
+        video_countries = self.video_countries[client_id]
+        video_countries.setdefault(video_id, [])
+
+        trending_dates = video_ids[video_id]
+        trending_countries = video_countries[video_id]
 
         if (not country in trending_countries):
             trending_countries.append(country)
@@ -20,7 +50,8 @@ class ThumbnailGrouper:
         if (not trending_date in trending_dates):
             trending_dates.append(trending_date)
 
-        if (len(trending_countries) >= 11 and len(trending_dates) >= 21):
+        if (len(trending_countries) >= country_count and len(trending_dates) >= MIN_PERIOD_IN_DAYS and video_id not in processed):
+            processed.add(video_id)
             return True
 
         return False
