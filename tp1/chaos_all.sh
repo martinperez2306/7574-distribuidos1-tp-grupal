1010fd38bce2c1e160739cdc22c74c809fd9bb1d
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-12-03 22:49:31 -0300

    Add more chaos script

---
+++ tp1/chaos_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+usage="Debe ingresar la frecuencia de caos (en segundos)"
+
+if [ $# -eq 0 ]; then
+  echo "$usage"
+  exit 1    
+fi
+
+KILL_FRECUENCY=$1
+
+./chaos.sh $KILL_FRECUENCY '^/acceptor$' '^/downloader$' '^/tag_unique$' '^/thumbnail_router$' '^/trending_router$' '^/trending_top$' '^/thumbnail_[[:digit:]]+$' '^/joiner_[[:digit:]]+$' '^/dropper_[[:digit:]]+$' '^/like_filter_[[:digit:]]+$'
